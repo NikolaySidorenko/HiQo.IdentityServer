a7b36a84241777f498ad7906ab0cddd173649a75
NikolaySidorenko <nikolaysidorenko99@gmail.com>
2018-12-13 19:27:09 +0300

    add mvc client configure authorization

---
+++ HiQo.MvcClient/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HiQo.MvcClient.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
